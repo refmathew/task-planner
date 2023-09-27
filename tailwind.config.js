65100399636dae2c2d6ddaeaa1edaa99db4db687
Mathew Reforma <reformamathew@protonmail.com>
2023-09-27 19:37:37 +0800

    Add changes to tailwind and custom css libraries

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,11 @@ export default {
       colors:{
         "primary": "#1560f6",
         "secondary": "#1e272e",
+        "success": "#34d188",
+        "danger": "#ff4e66",
         "red": "#ff5e57", 
-        "white": "#fff",
+        "white": "#ffffff",
+        "black": "#000000",
         "gray-50": "#f9fafc",
         "gray-100": "#f3f4f7",
         "gray-200": "#e8e9ee",
@@ -25,6 +28,20 @@ export default {
         "gray-900": "#3a3b45",
         
       },
+      spacing:{
+        '2': '.2rem',
+        '4': '.4rem',
+        '6': '.6rem',
+        '8': '.8rem',
+        '16': '1.6rem',
+        '20': '2rem',
+        '24': '2.4rem',
+        '32': '3.2rem',
+        '64': '6.4rem'
+      },
+      borderRadius: {
+        DEFAULT: '.4rem',
+      }
     },
   },
 };
